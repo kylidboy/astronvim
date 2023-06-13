203142c1ccfb121257740245f9410ab8c41ddb47
Kyli Bing <kylidboy@outlook.com>
2023-06-13 14:08:32 +0800

    feat: tmux fusion

--- plugins/user.lua
+++ plugins/user.lua
@@ -14,4 +14,17 @@ return {
     ft = "yul",
     -- event = "BufEnter *.yul",
   },
+  {
+    "alexghergh/nvim-tmux-navigation",
+    lazy = false,
+    opts = {
+      disable_when_zoomed = true,
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+      },
+    },
+  },
 }
